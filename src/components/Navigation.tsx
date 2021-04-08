b66aca37436c427fc9c526ce5bc36410d1a321d8
dmarceo316 <arceo.adam@gmail.com>
2021-04-08 14:02:17 -0700

    "added new display component with material UI"

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+// import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      display: "flex"
+    },
+    navColor: {
+      backgroundColor: "#222",
+      color: "#999"
+    }
+  }),
+);
+
+// const theme = createMuiTheme({
+//   palette: {
+//     primary: {
+//       light: '#757ce8',
+//       main: '#3f50b5',
+//       dark: '#333333',
+//       contrastText: '#fff',
+//     },
+//     secondary: {
+//       light: '#ff7961',
+//       main: '#f44336',
+//       dark: '#ba000d',
+//       contrastText: '#000',
+//     },
+//   },
+// });
+
+
+
+export function NavigationBar(){
+    const classes = useStyles();
+
+    return (
+        <div>
+            <AppBar position="static" className={classes.navColor}>
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                    Machette
+                    </Typography>
+                </Toolbar>
+                </AppBar>
+        </div>
+    )
+}
